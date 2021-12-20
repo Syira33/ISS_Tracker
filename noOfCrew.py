40f25636a6fe31f1151cf8ffe60a42690b6ce628
Syira33 <96217633+Syira33@users.noreply.github.com>
2021-12-20 08:33:23 +0800

    Enable view on the web browser

--- noOfCrew.py
+++ noOfCrew.py
@@ -1,5 +1,60 @@
 import json, urllib.request, time
+import webbrowser
 
+f = open('index.html','w')
+
+message = """<html>
+
+  <head>
+    <title>ISS Location Tracker</title>
+    <link rel="stylesheet" href="styles.css" type="text/css" />
+    <!-- CSS for our Map (Mandatory) -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
+  </head>
+  <body>
+
+    <div>
+        <h1>ISS Location Tracker</h1>
+  
+        <!-- Map goes here -->
+        <div id="map-div">
+  
+        </div>
+  
+        <!-- Details about ISS -->
+        <div id="details">
+          <!-- Time -->
+          <div>
+            Time:
+            <span class="time"></span>
+          </div>
+          <!-- Latitude -->
+          <div>
+            Latitude:
+            <span class="latitude"></span>
+          </div>
+          <!-- Longitude -->
+          <div>
+            Longitude:
+            <span class="longitude"></span>
+          </div>
+          
+        </div>
+        <p 
+      </div>
+
+
+    <!-- Javascript for the Map -->
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
+    <script src="script.js" type="text/javascript"></script>
+  </body>
+
+</html>"""
+
+f.write(message)
+f.close()
+
+webbrowser.open_new_tab('index.html')
 
 #A first JSON request to retrieve the name of all the astronauts currently in space.
 url = "http://api.open-notify.org/astros.json"
